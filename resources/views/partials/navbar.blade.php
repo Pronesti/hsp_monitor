51e1ab68bb23d1855db475b321e13666e229801b
matias <matias.pagano07@gmail.com>
2021-11-10 14:16:34 -0300

    cambio url sonarqube

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,7 +17,7 @@
                 <a target="_blank" href="https://one.newrelic.com/launcher/dashboards.launcher?pane=eyJuZXJkbGV0SWQiOiJkYXNoYm9hcmRzLmRhc2hib2FyZCIsImVudGl0eUlkIjoiTmpjMU5EVXlmRlpKV254RVFWTklRazlCVWtSOE1UTTVNekF6TWciLCJ1c2VEZWZhdWx0VGltZVJhbmdlIjpmYWxzZSwiaXNUZW1wbGF0ZUVtcHR5IjpmYWxzZX0=&platform[$isFallbackTimeRange]=false" class="btn btn-dark">New Relic</a>
             </li>
             <li class="nav-item">
-                <a target="_blank" href="http://10.9.1.110:9000/dashboard?id=CMS_GO_PROVIDERS" class="btn btn-dark">SonarQube</a>
+                <a target="_blank" href="http://10.11.186.249:9000/projects?sort=-analysis_date" class="btn btn-dark">SonarQube</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle text-white" href="#" data-toggle="dropdown">
